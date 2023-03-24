40ce95ad8f28b3168415a9634a13364199fe3998
EduSchmid <121768226+EduSchmid@users.noreply.github.com>
2023-03-23 22:05:16 -0300

    Add files via upload

---
+++ Atividade/Clientes.cs
@@ -0,0 +1,19 @@
+namespace Atividade
+{
+    class Clientes
+    {
+        public string nome {get; set;}
+        public string endereco {get; set;}
+        public float valor {get; protected set;}
+        public float valor_imposto {get; protected set;}
+        public float valor_total {get; protected set;}
+
+        public virtual void Pagar_imposto(float v)
+        {
+            this.valor = v;
+            this.valor_imposto = this.valor * 10/100;
+            this.valor_total = this.valor + this.valor_imposto;
+        }
+
+    }
+}
---
+++ Atividade/Pessoa_Juridica.cs
@@ -0,0 +1,17 @@
+namespace Atividade
+
+{
+    class Pessoa_Juridica : Clientes
+    {
+        public string cnpj {get; set;}
+        public string ie {get; set;}
+
+        public override void Pagar_imposto(float v)
+        {
+            this.valor = v;
+            this.valor_imposto = this.valor * 20/100;
+            this.valor_total = this.valor + this.valor_imposto;
+        }
+        
+    }
+}
